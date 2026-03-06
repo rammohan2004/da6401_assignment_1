30bb5ba17405ce760f6e4861527f4e135faeccea
Ram mohan <ramamohanraobotla12@gmail.com>
2026-03-06 18:06:10 +0530

    update

--- src/ann/neural_layer.py
+++ src/ann/neural_layer.py
@@ -1,3 +1,5 @@
+
+
 import numpy as np
 
 class NeuralLayer:
@@ -8,7 +10,7 @@ class NeuralLayer:
             scaler = (2 / (input_nodes + output_nodes))**(0.5)
             self.W = np.random.randn(input_nodes, output_nodes) * scaler
             
-        # FIX: Make bias 1D array instead of 2D to avoid autograder shape mismatch
+        # 1D array for bias
         self.b = np.zeros(output_nodes) 
         self.grad_W = np.zeros((input_nodes, output_nodes))
         self.grad_b = np.zeros(output_nodes)
@@ -16,16 +18,12 @@ class NeuralLayer:
 
     def forward(self, X):
         self.X = X
-        # Defense: Ensure b is 1D with correct shape if autograder overrides weights
-        if self.b.shape[0] != self.W.shape[1]:
-            self.b = np.zeros(self.W.shape[1])
-            
+        # Just pure math! No resetting the bias here.
         Z = X @ self.W + self.b
         return Z
 
     def backward(self, grad_output):
         self.grad_W = (self.X).T @ grad_output
-        # FIX: Remove keepdims=True so grad_b is 1D
         self.grad_b = np.sum(grad_output, axis=0) 
         grad_input =  grad_output @ (self.W).T
         return grad_input
